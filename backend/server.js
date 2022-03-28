59701822e73c0574cc3ac9dac87dc9496b494ae3
adityatm28 <adityatm28@gmail.com>
2022-03-28 12:17:42 +0530

    api call of products

--- backend/server.js
+++ backend/server.js
@@ -14,5 +14,5 @@ app.get('/', (req, res) => {
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`Server at http//:localhost:{port}`);
+  console.log(`Server at http//:localhost:${port}`);
 });
