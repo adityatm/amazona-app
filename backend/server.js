e73c7a064d61f11dcddcced145e9bca8028f5139
adityatm28 <adityatm28@gmail.com>
2022-04-11 13:22:20 +0530

    connect mongodb, create user and signin

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,22 @@
 import express from 'express';
 import data from './data.js';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import userRouter from './routers/userRouter.js';
 
+dotenv.config();
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose
+  .connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/amazona', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true,
+  })
+  .then(() => console.log('connection successfully...'))
+  .catch((err) => console.log(err));
 
 app.get('/api/products/:id', (req, res) => {
   const product = data.products.find((x) => x._id === req.params.id);
@@ -18,6 +33,15 @@ app.get('/api/products', (req, res) => {
 
 app.get('/', (req, res) => {
   res.send('server is ready');
+});
+
+app.use('/api/users', userRouter);
+app.get('/', (req, res) => {
+  res.send('Server is ready');
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
 });
 
 // eslint-disable-next-line no-undef
